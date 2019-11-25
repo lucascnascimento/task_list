f48c895b35a57e08b8a3e5bf88e48c541be26311
Lucas Cordeiro <lucascnascimento@gmail.com>
2019-11-25 15:53:22 -0300

    Delte task from Local storage implemented

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ loadEventListeners()
 
 // Load all event listeners
 function loadEventListeners(){
+  // DOM load event
+  document.addEventListener('DOMContentLoaded', getTasks)
   // Add task event
   form.addEventListener('submit', addTask)
   // Remove task event
@@ -19,6 +21,36 @@ function loadEventListeners(){
   clearBtn.addEventListener('click', clearTasks)
   // Filter tasks event
   filter.addEventListener('keyup', filterTasks)
+}
+
+// Get Tasks from Local Storage
+function getTasks(){
+  let tasks
+  if(localStorage.getItem('tasks') === null){
+    tasks=[]
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'))
+  }
+
+  tasks.forEach(function(task){
+    //Create li element
+    const li = document.createElement('li')
+    //Add class
+    li.className='collection-item'
+    // Create text node and append to li
+    li.appendChild(document.createTextNode(task))
+    // Create new link element
+    const link = document.createElement('a')
+    // Add class
+    link.className = 'delete-item secondary-content'
+    // Add icon html
+    link.innerHTML = '<i class="fa fa-remove">x</i>'
+    // Append the link to li
+    li.appendChild(link)
+
+    // Append li to ul
+    taskList.appendChild(li)
+  })
 }
 
 // Add task
@@ -45,10 +77,26 @@ function addTask(e){
   // Append li to ul
   taskList.appendChild(li)
 
+  // Store in Local Storage
+  storeTaskInLocalStorage(taskInput.value)
+
   // Clear input
   taskInput.value = ''
 
   e.preventDefault();
+}
+
+// Store Task
+function storeTaskInLocalStorage(task){
+  let tasks
+  if(localStorage.getItem('tasks') === null){
+    tasks=[]
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'))
+  }
+
+  tasks.push(task)
+  localStorage.setItem('tasks', JSON.stringify(tasks))
 }
 
 // Remove Task
@@ -56,8 +104,29 @@ function removeTask(e) {
   if(e.target.parentElement.classList.contains('delete-item')){
     if(confirm('Are You Sure?')){ 
      e.target.parentElement.parentElement.remove()
+
+     // Remove from LS
+     removeTaskFromLocalStorage(e.target.parentElement.parentElement)
     } 
   }
+}
+
+// Remove from Local Storage
+function removeTaskFromLocalStorage(taskItem){
+  let tasks
+  if(localStorage.getItem('tasks') === null){
+    tasks=[]
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'))
+  }
+
+  tasks.forEach(function(task, index){
+    if(taskItem.textContent === task){
+      tasks.splice(index, 1);
+    }
+  })
+
+  localStorage.setItem('tasks', JSON.stringify(tasks))
 }
 
 // Clear Tasks
